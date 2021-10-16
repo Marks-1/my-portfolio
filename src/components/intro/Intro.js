ccd570568389ad0525fcf550f579c56cd27b2012
Marks-1 <onyachmark1@gmail.com>
2021-10-16 06:08:11 +0300

    vscode font-size settings "15"

---
+++ src/components/intro/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Intro.css'
+
+const Intro = () =>{
+  return(
+    <div className="my-intro">
+      <div className="my-intro__left">Left</div>
+      <div className="my-intro__right">Right</div>
+    </div>
+  );
+}
+
+export default Intro
